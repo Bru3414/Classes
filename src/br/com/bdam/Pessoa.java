accf2dbc7df94b218df25207dbb1bc8feb75b46a
Bruno Dambroski <bruno3414.dambroski@gmail.com>
2024-09-24 08:09:18 -0400

    first commit

---
+++ src/br/com/bdam/Pessoa.java
@@ -0,0 +1,7 @@
+package br.com.bdam;
+
+public abstract class Pessoa {
+
+	private String nome;
+	private int idade;
+}
---
+++ src/br/com/bdam/PessoaFisica.java
@@ -0,0 +1,15 @@
+package br.com.bdam;
+
+public class PessoaFisica extends Pessoa {
+	
+	private Long cpf;
+	
+	public Long getCpf() {
+		return cpf;
+	}
+	
+	public void setCnpj(Long cpf) {
+		this.cpf = cpf;
+	}
+	
+}
